77364fe5a599536b0e2a51c7eaf71fa7649b9f02
Attila0604 <141563166+Attila0604@users.noreply.github.com>
2026-05-16 19:32:22 +0200

    Update page.tsx

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -3,7 +3,7 @@ import { createAdminClient } from "@/lib/supabase/admin";
 import { redirect } from "next/navigation";
 
 export default async function MePage() {
-  const supabase = createClient();
+  const supabase = await createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
